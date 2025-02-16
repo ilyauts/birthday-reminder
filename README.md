320438ac4283b15d272f209e7f153ec9121580ba
This Guy Studios <ilya.uts@gmail.com>
2025-02-16 15:05:34 -0500

    Add main script and readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # birthday-reminder
+
+<img src="./logo.webp" style="width: 50%; margin: 20px 25%;"> 
+
 With the fall of Facebook, it has become increasing difficult to keep track of friends' birthdays. This simple script helps seamlessly port data from a Google Sheet that it used as a DB and remind you of your friends' biggest life events!
+
+## Parameters
+`SPREADSHEET_ID` needs to be set within the `script.js` file.
+
+
+## Instructions
+
+ ### Setting up the spreadsheet
+
+ To get you started, we've created a [template spreadsheet](https://docs.google.com/spreadsheets/d/1DsexhFqRNXAa-tO0u2okr7DKONtwBisNXoht7wAIq74/) which you can clone and use as the database for this project.
+ 
+ All birthdays in the DB are expected to be in the form of MM/DD/YYYY. If you happen to not know any of the requiste information for the birthday, feel free to enter `????`. As an example, if I didn't know the year of someone's birthday, but I knew the month and day, I'd enter: 02/02/????. The script automatically ignores any birthdays with the `????` substring.
+
+ ### Setting up the script
+
+ Once the spreadsheet is seeded with all of the birthdays that you need (at least for now), go to [AppScript](https://script.google.com/home) and create a New project.
+
+ On the left side, under `Services`, add `Calendar` (version v3) and `Sheets` (version v4).
+
+ Then paste the contents of `script.js` and save (ctrl + s).
+
+ [Find your DB's spreadsheet ID](https://stackoverflow.com/questions/36061433/how-do-i-locate-a-google-spreadsheet-id) and paste it in the script (replacing the contents of `SPREADSHEET_ID`);
+
+ Finally, switch the function to execute on the top dropdown to `main` and click `Run`.
+
+ If all the data and integration was set up correctly, you should now have calendar events!
---
+++ script.js
@@ -0,0 +1,226 @@
+/**
+ * CONSTANTS TO CHANGE
+ */
+const SPREADSHEET_ID = 'REPLACE_ME'; // Change this to your spreadsheet ID
+const UNKNOWN_CHARS = '????'; // Birthdays will feature this string is some part of them is unknown
+const CALENDAR_ID = 'primary';
+
+
+/**
+ * Read the data from the spreadsheet DB
+ */
+function readBirthdays() {
+  try {
+    console.log('Pulling birthday from Google Sheets Database...')
+    const result = Sheets.Spreadsheets.Values.get(SPREADSHEET_ID, 'A:Z');
+
+    // For now we only care about the first two columns (name and birthday)
+    const birthdays = result.values.map(v => [v[0], v[1]]).filter(v => v[1] && !v[1]?.includes(UNKNOWN_CHARS));
+
+    console.log('Birthdays processing completed...')
+    return birthdays;
+  } catch (err) {
+    console.log('Failed with error %s', err.message);
+  }
+}
+
+/**
+ * Checks if the birthday already exists for this person 
+ */
+function duplicateBirthdayEntry(birthday) {
+  const [birthdayName, birthdate] = birthday; 
+  const fullBirthdayEventName = birthdayNameWrapper(birthdayName);
+  
+  try {
+    // Get Events for this day
+    const nextBirthdate = findNextBirthday(birthdate);
+    const startDate = getRelativeDate(nextBirthdate, 0, 0);
+    const endDate = getRelativeDate(nextBirthdate, 1, 0);
+
+    const events = Calendar.Events.list(
+      CALENDAR_ID,{
+      timeMin: startDate.toISOString(),
+      timeMax: endDate.toISOString()
+    });
+
+    for(const item of events.items) {
+      if(item.summary === fullBirthdayEventName) {
+        console.log(`Duplicate found: ${item.summary}, skipping...`);
+        return true;
+      }
+    }
+
+    console.log(`No matches found for ${fullBirthdayEventName}`);
+    return false;
+  } catch (err) {
+    console.log('Failed with error %s', err.message);
+  }
+
+  return true;
+}
+
+/**
+ * Creates a birthday for the person
+ */
+function createBirthday(birthday) {  
+  const [birthdayName, birthdate] = birthday; 
+  const fullBirthdayEventName = birthdayNameWrapper(birthdayName);
+
+  const nextBirthday = findNextBirthday(birthdate);
+  const followingDate = getRelativeDate(new Date(nextBirthday), 1, 0);
+
+  const nextBirthdayString = convertToStandardDateFormat(nextBirthday);
+  const followingDateString = convertToStandardDateFormatFromObj(followingDate);
+  
+  console.log(`Creating new birthday entry for ${birthdayName}...`);
+
+  let event = {
+    summary: fullBirthdayEventName,
+    start: {
+      date: nextBirthdayString,
+    },
+    end: {
+      date: followingDateString,
+    },
+    attendees: [      
+    ],
+    // Grape background. Use Calendar.Colors.get() for the full list.
+    colorId: 3,
+    eventType: 'birthday',
+    reminders: {
+      useDefault: false,
+      overrides: [{
+        method: 'email',
+        minutes: 0
+      }, {
+        method: 'popup',
+        minutes: 0
+      }]
+    },
+    recurrence: [
+      'RRULE:FREQ=YEARLY;WKST=MO;'
+    ],
+    transparency: 'transparent',
+    visibility: 'private'
+  };
+
+  try {
+    // call method to insert/create new event in provided calandar
+    event = Calendar.Events.insert(event, CALENDAR_ID);
+    console.log('Event ID: ' + event.id, event);
+  } catch (err) {
+    console.log('Failed with error %s', err.message);
+  }
+}
+
+/**
+ * Helper function to get a new Date object relative to the current date.
+ * @param {number} daysOffset The number of days in the future for the new date.
+ * @param {number} hour The hour of the day for the new date, in the time zone
+ *     of the script.
+ * @return {Date} The new date.
+ */
+function getRelativeDate(baseDate, daysOffset, hour) {
+  const date = new Date(baseDate);
+
+  date.setDate(date.getDate() + daysOffset);
+  date.setHours(hour);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date;
+}
+
+/**
+ * Helper function to get a new Date object relative to the current date.
+ * @param {number} month of the birthday.
+ * @param {number} day of the birthday.
+ * @param {number} year of the birthday.
+ * @return {[Date, Date]} The new start and end date.
+ */
+function fullDay(month, day, year) {
+  const date = new Date();
+  date.setFullYear(year);
+  date.setMonth(month-1);
+  date.setDate(day);
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+
+  const endDate = new Date(date);
+  endDate.setDate(endDate.getDate() + 1)
+
+  return [date, endDate];
+}
+
+/**
+ * Birthday wrapper function
+ */
+function birthdayNameWrapper(name) {
+  return `${name}'s Birthday!`;
+}
+
+/** 
+ * Convert to a Google Calendar Understandable Date, from DB date
+ */
+function convertToStandardDateFormat(date) {
+  const dateArr = date.split('/');
+
+  const newDateArr = [dateArr[2], dateArr[0], dateArr[1]];
+  return newDateArr.join('-');
+}
+
+/** 
+ * Convert to a Google Calendar Understandable Date, from Date Obj
+ */
+function convertToStandardDateFormatFromObj(date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const dateString = `${year}-${month}-${day}`;
+  return dateString;
+}
+
+/** 
+ * Determine next birthdate
+ */
+function findNextBirthday(date) {
+  const dateArr = date.split('/');
+
+  const currDate = new Date();
+  const currDateYear = currDate.getFullYear();
+
+  dateArr[2] = currDateYear;
+
+  const tempDate = new Date(dateArr[2], dateArr[0], dateArr[1]);
+
+  // Ensure that we're only looking at birthdays that haven't happened this year
+  if(currDate <= tempDate) {
+    return dateArr.join('/');
+  } else {
+    // Otherwise choose it for next year
+    dateArr[2] = currDateYear + 1;
+    return dateArr.join('/');
+  }
+}
+
+/** 
+ * Controller
+ */
+function main() {
+  console.log('Kicked off processing')
+  const birthdayArray = readBirthdays();
+
+  for(const birthday of birthdayArray) {
+    // Skip the header row
+    if(birthday[0] === 'Name') {
+      continue;
+    }
+
+    console.log(`Veryfing existence of ${birthdayNameWrapper(birthday[0])}...`);
+    if(!duplicateBirthdayEntry(birthday)) {
+      createBirthday(birthday)
+    }
+  }
+}
